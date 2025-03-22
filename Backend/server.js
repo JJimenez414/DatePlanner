71964756b48a24886c7e058fb9e37a12869ee90c
Jose <joseangel130414@gmail.com>
2025-03-21 22:53:06 -0500

    Set up the server Socket

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,28 @@
 const express = require('express');
-const cors = require('cors')
-const { createServer } = require('http')
+const cors = require('cors');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
 const PORT = 3000;
 const app = express();
 const bodyParser = require('body-parser');
 const httpServer = createServer(app) // createServer expects a "requestlister" which express is
-
-// setting up cors
-const whitelist = ['http://localhost:5173'];
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) != -1 || !origin){
-            callback(null, true);
-        } else {
-            callback(new Error("origin is not allowed"), false);
-        }
+const io = new Server(httpServer, { // creates a io instance
+    cors: {
+        origin: "http://localhost:5173/"
     }
-}
+});
 
-app.use(cors(corsOptions))
+io.on("connection", (socket) => {
+    socket.emit("hello", "world");
+
+    socket.on("hello", (arg) => {
+        console.log(arg);
+    });
+});
+
 app.use(bodyParser.json())
 
-app.post('/', (req, res) => {
-    data = req.body;
-    res.send({data: data.data});
-});
-
-app.listen(PORT, () => {
-    console.log(`Listening to port ${PORT}`)
-});
+httpServer.listen(PORT, () => {
+    console.log(`Listening to port: ${PORT}`)
+} )
