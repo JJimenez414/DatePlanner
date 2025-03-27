5b8be38b9da6d1f16eb09836722af2d259be696f
Jose <joseangel130414@gmail.com>
2025-03-26 23:15:29 -0500

    managed to have initial state of draging animation.

--- Frontend/DatePlanner/src/components/ActivityCard.jsx
+++ Frontend/DatePlanner/src/components/ActivityCard.jsx
@@ -1,11 +1,37 @@
-import { useRef } from 'react';
+
+import { motion, useAnimation } from "framer-motion";
+import { useState } from "react";
 
 function ActivityCard({ activity = "" }) {
 
+    const controls = useAnimation();
+    const [swiped, setSwiped] = useState(false);
+    const [swipeRight, setSwipeRight] = useState();
+
+    const handleDragEnd = (_, info) => {
+
+        if (Math.abs(info.offset.x) > 100) {
+            if (info.offset.x < 0)  {
+                setSwipeRight(true);
+            } else {
+                setSwipeRight(false);
+            }
+            setSwiped(true);
+
+            controls.start({ x : swipeRight ? 300 : -300, opacity : 0, rotate : swipeRight ? 20 : -20})
+        }  else {
+            controls.start ({x : 0, rotate : 0})
+        }
+    }
+
+    if (swiped) return null;
+
     return (
-        <div className="activity-card center">
+
+        <motion.div className="activity-card center" drag="x" dragConstraints={{left : -50, right : 50}} onDragEnd={handleDragEnd} initial={{x: 0, rotate: 0}} transition={{type: "spring", stiffness: 300, damping: 20}}>
             <p>{activity}</p>
-        </div>
+        </motion.div>
+
     );
 }
 
